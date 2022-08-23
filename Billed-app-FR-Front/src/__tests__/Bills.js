7d594bbb12725874c3e341db711d578d949f9060
ERIC CLERMONTET <emc@go4.social>
2022-08-23 13:59:01 +0200

    Tests U+I - 80% Bills Coverage in Statement

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,13 +2,17 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {screen, waitFor, fireEvent} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import {bills} from "../fixtures/bills.js"
-import {ROUTES_PATH} from "../constants/routes.js";
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
+import Bills from "../containers/Bills.js"
 
 import router from "../app/Router.js";
+
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -28,6 +32,7 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
       expect(windowIcon.classList.contains('active-icon')).toBe(true)
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -36,4 +41,40 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+})
+
+
+describe("When on the Dashboard and I click on an eye icon", () => {
+  test("Then a modal should open", async () => {
+
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({pathname})
+    }
+
+describe("Given I am connected as an employee", () => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))
+
+    const billContainer = new Bills({
+      document, 
+      onNavigate, 
+      store: null, 
+      localStorage: localStorageMock
+    })
+
+    const html = BillsUI({ data: bills })
+    document.body.innerHTML = html
+
+    $.fn.modal = jest.fn();
+    const handleClickIconEye  = jest.fn((e) => billContainer.handleClickIconEye(e.target))
+
+    const eyeIcon = screen.getAllByTestId('icon-eye')[0]
+    eyeIcon.addEventListener('click', handleClickIconEye)
+    fireEvent.click(eyeIcon)
+
+    expect(handleClickIconEye).toHaveBeenCalled()
+  })
+})
 })
